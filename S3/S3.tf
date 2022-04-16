41337be2b727f31a7adf91934ad8f60d8f5c9c00
rahulpdiggi <78218847+rahulpdiggi@users.noreply.github.com>
2022-04-16 17:32:30 +0530

    Delete S3 directory

--- S3/S3.tf
+++
@@ -1,9 +0,0 @@
-# This finds the folder AWS and gets keys
-provider "aws" {
-  region = "ap-south-1"
-}
-#we will create a S3 bucket
-resource "aws_s3_bucket" "bucket_from_terraform" {
-  bucket = "tfbucket2312312e3"
-  aws_s3_bucket_acl = "private"
-}
